2a187866afc759a6072cbb9c7e707d913b20903a
ali.dag <ali.dag@getir.com>
2024-03-02 23:45:51 +0300

    geojson vis

--- nichemap/src/App.js
+++ nichemap/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-import {MapContainer, TileLayer} from 'react-leaflet'
+import {MapContainer, TileLayer, GeoJSON} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-
+import poly from './polygon.json'
 
 function App() {
+  const setColor = ({ properties }) => {
+    return { weight: 1 };
+  };
   return (
      <MapContainer center={[41.017860343158034, 28.997707316618385]} zoom={13}>
        <TileLayer 
           url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
        />
+       <GeoJSON data={poly} style={setColor} />
      </MapContainer>
   );
 }

36f295b36fc31538e5119bbfd28c14c091f7b843
ali.dag <ali.dag@getir.com>
2024-03-02 21:08:50 +0300

    extra char removed

--- nichemap/src/App.js
+++ nichemap/src/App.js
@@ -5,7 +5,7 @@ import 'leaflet/dist/leaflet.css'
 
 function App() {
   return (
-     <MapContainer center={[41.017860343158034, 28.997707316618385]} zoom={13}>>
+     <MapContainer center={[41.017860343158034, 28.997707316618385]} zoom={13}>
        <TileLayer 
           url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
